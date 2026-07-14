df6832d4dba4118181a8f1600be9551d5d849b5b
teatror99 <149262897+TeaTror99@users.noreply.github.com>
2026-07-14 08:55:12 -0500

    feat(sql): configure new SQL parser crate

---
+++ crates/wackdb_sql/src/lib.rs
@@ -0,0 +1,9 @@
+#![allow(missing_docs)]
+#![allow(clippy::missing_panics_doc)]
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::too_many_lines)]
+#![allow(clippy::unwrap_used)]
+#![allow(clippy::redundant_closure_for_method_calls)]
+
+pub mod parser;
+pub use parser::*;
